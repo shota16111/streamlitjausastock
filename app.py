e3d6adb3d0c3e7ae8f2575a1b0f528c8278d76d6
shota16111 <82205813+shota16111@users.noreply.github.com>
2023-06-08 07:48:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import yfinance as yf
 import altair as alt
 import streamlit as st
 
-st.title('日本と米国の株価可視化アプリ')
+st.title('日本と米国のaaaaaaaaaaaaaaaaaaaaa株価可視化アプリ')
 st.title('会社名いじればエラー直ります')
 
 st.sidebar.write("""
